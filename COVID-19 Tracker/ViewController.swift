a814c88d55e5d811bba271304b84e3aaf20e4b30
Aidos Mukatayev <mukatayev19@gmail.com>
2020-12-14 14:33:51 +0900

    Initial Commit

---
+++ COVID-19 Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19 Tracker
+//
+//  Created by AZM on 2020/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
